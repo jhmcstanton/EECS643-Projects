ac3bd68d4b8a6916141d3fd5a3cbc7a6fbdef687
Jim Stanton <jim@jhstanton.com>
2015-10-20 11:25:23 -0500

    Fixed segfault in test (implementation was fine)

--- regfile/regfile.c
+++ regfile/regfile.c
@@ -1,8 +1,35 @@
 #include "regfile.h"
-
+#include <stdlib.h>
+#include<stdio.h>
+#include<time.h>
 
 int main(){
+  srand(time(NULL));
+  // Testing the RegFile
+  uint32_t *s_data = (uint32_t *) malloc(sizeof(uint32_t)), *t_data = (uint32_t *) malloc(sizeof(uint32_t)); 
+  uint32_t i, write_data; 
 
+  reg_addr rs, rt, rd;
+  bool write_en;
+  // Fill it up
+  for(i = 0; i < REGFILESIZE; i++){
+    printf("i: %u\n", i);
+    RegFile(0, 0, (reg_addr) i, rand(), 1, s_data, t_data);
+  }
+  // Do some random testing (reads and writes)
+  for(i = 0; i < (REGFILESIZE * 4); i++){
+    rs = rand();
+    rt = rand();
+    rd = rand();
+    write_en = rand() % 2;
+    write_data = rand();
+    printf("RegFile(%u, %u, %u, %u, %u, s_data, t_data) => ", rs, rt, rd, write_data, write_en);
+    RegFile(rs, rt, rd, write_data, write_en, s_data, t_data);
+    printf("s_read: %d, t_read: %d\n", *s_data, *t_data);
+  }
+  
+  free(s_data);
+  free(t_data);
   return 0;
 }
 
@@ -20,7 +47,9 @@ void RegFile(reg_addr rs, reg_addr rt, reg_addr rd, uint32_t wrt_data, bool wrt_
     reg_file[rd] = wrt_data;
   }
   // Second half data is retrieved. Data is *always* read
+  //s_data = &reg_file[rs];
   *s_data = reg_file[rs];
   *t_data = reg_file[rt];
+  //t_data = &reg_file[rt];
   return;
 }
