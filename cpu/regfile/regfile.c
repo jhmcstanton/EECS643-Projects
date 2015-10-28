c285695c2edb0fd4b620b63f736442b95be4c4a5
Jim Stanton <jim@jhstanton.com>
2015-10-28 17:51:13 -0500

    Removed main from regfile.c into its own test file

--- cpu/regfile/regfile.c
+++ cpu/regfile/regfile.c
@@ -3,35 +3,7 @@
 #include<stdio.h>
 #include<time.h>
 
-int main(){
-  srand(time(NULL));
-  // Testing the RegFile
-  uint32_t *s_data = (uint32_t *) malloc(sizeof(uint32_t)), *t_data = (uint32_t *) malloc(sizeof(uint32_t)); 
-  uint32_t i, write_data; 
-
-  reg_addr rs, rt, rd;
-  bool write_en;
-  // Fill it up
-  for(i = 0; i < REGFILESIZE; i++){
-    printf("i: %u\n", i);
-    RegFile(0, 0, (reg_addr) i, rand(), 1, s_data, t_data);
-  }
-  // Do some random testing (reads and writes)
-  for(i = 0; i < (REGFILESIZE * 4); i++){
-    rs = rand();
-    rt = rand();
-    rd = rand();
-    write_en = rand() % 2;
-    write_data = rand();
-    printf("RegFile(%u, %u, %u, %u, %u, s_data, t_data) => ", rs, rt, rd, write_data, write_en);
-    RegFile(rs, rt, rd, write_data, write_en, s_data, t_data);
-    printf("s_read: %d, t_read: %d\n", *s_data, *t_data);
-  }
-  
-  free(s_data);
-  free(t_data);
-  return 0;
-}
+uint32_t reg_file[REGFILESIZE];
 
 /*
   Register File that takes 1 cycle to run. Contains @REGFILESIZE registers.
